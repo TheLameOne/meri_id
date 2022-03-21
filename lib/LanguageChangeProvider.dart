c69c28a6f7ef80d2c16626af814bf699906dd856
Harsh Verma <vermaharsh463@gmail.com>
2022-03-22 02:14:57 +0530

    addition of hindi language in app

---
+++ lib/LanguageChangeProvider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class LanguageChangeProvider with ChangeNotifier {
+  Locale _currentLocale = const Locale("en");
+  Locale get currentLocale => _currentLocale;
+
+  void changeLocale(String _locale) {
+    this._currentLocale = new Locale(_locale);
+    notifyListeners();
+  }
+}
