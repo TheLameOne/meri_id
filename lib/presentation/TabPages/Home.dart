4126379fbb4606ca91e5e50450529f03c0df9678
Kartikeya Sharma <kartikeyasharma@Kartikeyas-MacBook-Pro.local>
2022-08-13 21:09:30 +0530

    url launcher enabled

--- lib/presentation/TabPages/Home.dart
+++ lib/presentation/TabPages/Home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:meri_id/presentation/custom/CustomCard.dart';
 import 'package:meri_id/presentation/features/GoogleMapTracking.dart';
 import 'package:meri_id/services/widgets/CustomText.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -26,12 +28,15 @@ class _HomeState extends State<Home> {
             CustomText.xLargeText("Home"),
             const SizedBox(height: 32),
             for (int i = 0; i < 10; i++)
-               CustomCard(
-                  name: "Harsh Verma",
-                  date: "22/09/2022",
-                  time: "3:00 pm",
-                  onTap: () {},
-                ),              
+              CustomCard(
+                name: "Harsh Verma",
+                date: "22/09/2022",
+                time: "3:00 pm",
+                onTap: () {},
+                makeCall: () {
+                 launchUrlString("tel:+91963852741"); 
+                },
+              ),
           ])),
     );
   }
--- lib/presentation/custom/CustomCard.dart
+++ lib/presentation/custom/CustomCard.dart
@@ -9,71 +9,75 @@ class CustomCard extends StatelessWidget {
   final String name;
   final Color color;
   final Function onTap;
+  final Function makeCall;
 
   const CustomCard(
-      {
-      this.booking_id = "7231905",
+      {this.booking_id = "7231905",
       this.date = "Date",
       this.time = "Time",
       this.name = "Person Name",
+      required this.makeCall,
       this.color = Styles.redColor,
-      required this.onTap 
-      });
+      required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 16),
-        child: Card(
-          elevation: 5,
-          shadowColor: Styles.blackColor,
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            side: BorderSide(
-              color: color,
-              width: 2,
-            ),
-            borderRadius: BorderRadius.circular(10),
+      child: Card(
+        elevation: 5,
+        shadowColor: Styles.blackColor,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: color,
+            width: 2,
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        // profile pic
-                        Icon(Icons.person),
-                        // Name
-                        CustomText.smallText(name),
-                      ],
-                    ),
-                    // Calling button
-                    IconButton(onPressed: () {}, icon: Icon(Icons.call))
-                  ],
-                ),
-                Row(
-                  children: [
-                    CustomText.smallText("Booking ID : "),
-                    CustomText.smallText(booking_id),
-                    // Booking ID
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Date
-                    CustomText.timeText(date),
-                    // Time
-                    CustomText.timeText(time),
-                  ],
-                )
-              ],
-            ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      // profile pic
+                      Icon(Icons.person),
+                      // Name
+                      CustomText.smallText(name),
+                    ],
+                  ),
+                  // Calling button
+                  IconButton(
+                      onPressed: () {
+                        makeCall();
+                      },
+                      icon: Icon(Icons.call))
+                ],
+              ),
+              Row(
+                children: [
+                  CustomText.smallText("Booking ID : "),
+                  CustomText.smallText(booking_id),
+                  // Booking ID
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Date
+                  CustomText.timeText(date),
+                  // Time
+                  CustomText.timeText(time),
+                ],
+              )
+            ],
           ),
         ),
+      ),
     );
   }
 }
